74628c40875266e063ccdbbce911edb2a81ef872
Farell <leonardo6131@gmail.com>
2021-11-03 15:54:00 +0300

    тестовая таблица

---
+++ REks2/UserData.lua
@@ -0,0 +1,61 @@
+local UserInfoLib = {}
+
+UserInfoLib.USER_DATA = {
+    user_1 = {
+        name 		= 'Мария Ивановна Пупкина',
+        age  		= '52',
+        gender 		= 'female',
+        profession 	= 'teacher',
+        country 	= 'Russia',
+        city		= 'Novosibirsk',
+        user_id  	= '284064781',
+        credit_card = '5177 4348 3833 5351',
+    },
+
+    user_2 = {
+        name 		= 'Romeo Garcia',
+        age  		= '37',
+        gender 		= 'male',
+        profession 	= 'actor',
+        country 	= 'Spain',
+        city		= 'Alicante',
+        user_id  	= '724701885',
+        credit_card = '5121 6425 6133 9493',
+    },
+
+    user_3 = {
+        name 		= 'Nuria Gonzales',
+        age  		= '24',
+        gender 		= 'female',
+        profession 	= 'accountant',
+        country 	= 'Spain',
+        city		= 'Palma',
+        user_id  	= '579364002',
+        credit_card = '4206 0008 7340 9423',
+    },
+
+    user_4 = {
+        name 		= 'Михаил Галанин',
+        age  		= '36',
+        gender 		= 'male',
+        profession 	= 'developer',
+        country 	= 'Great Britain',
+        city		= 'London',
+        user_id  	= '826013863',
+        credit_card = '5314 5088 6373 4341',
+    },
+
+    user_5 = {
+        name 		= 'Tashi Wong',
+        age  		= '28',
+        gender 		= 'female',
+        profession 	= 'masseur',
+        country 	= 'Thailand',
+        city		= 'Phatthaya',
+        user_id  	= '732802073',
+        credit_card = '5404 3613 2332 9489',
+    },
+}
+
+
+return UserInfoLib
